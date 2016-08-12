9da93d1c025ddf3e9c3618e5eda44063a1a6397f
Dan <dan.fujimura@gmail.com>
2016-08-12 15:45:03 +0900

    Add docker-compose.yml

--- apig-sample/db/db.go
+++ apig-sample/db/db.go
@@ -3,20 +3,22 @@ package db
 import (
 	"log"
 	"os"
-	"path/filepath"
+	//"path/filepath"
 	"strings"
 
 	"github.com/wantedly/apig-sample/models"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	// _ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/serenize/snaker"
 )
 
 func Connect() *gorm.DB {
-	dir := filepath.Dir("db/database.db")
-	db, err := gorm.Open("sqlite3", dir+"/database.db")
+	// dir := filepath.Dir("db/database.db")
+	// db, err := gorm.Open("sqlite3", dir+"/database.db")
+	db, err := gorm.Open("postgres", "host=localhost user=apig-sample dbname=apig-sample sslmode=disable password=mypassword")
 	if err != nil {
 		log.Fatalf("Got error when connect database, the error is '%v'", err)
 	}
